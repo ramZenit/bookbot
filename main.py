8f3811f1268fe91c01e0b84f9015b79f4b33a064
ramZenit <a.mb@outlook.it>
2025-08-12 12:51:42 +0200

    feat words count

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 def main():
     content = get_book_text("books/frankenstein.txt")
-    print(content)
+    print(count_words(content))
 
-
-
-
-
-
-
+def count_words(content):
+    words = content.split()
+    return f"{len(words)} words found in the document"
 
 def get_book_text(path_to_file):
     content = ""
